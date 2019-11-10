593b79d854df747d553bda53524b7782a877babe
wiii-na <arai.yu.wrp@gmail.com>
2019-11-10 18:27:51 +0900

    Initial Commit

---
+++ SimpleWebView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleWebView
+//
+//  Created by YuArai on 2019/11/10.
+//  Copyright © 2019 NaturalArchitect. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
